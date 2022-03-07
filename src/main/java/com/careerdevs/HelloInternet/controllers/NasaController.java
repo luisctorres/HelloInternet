9801667c864918b8123f7c5123f3e4b63630bdf7
luisctorres <Scratch2c@gmail.com>
2022-03-07 18:04:25 -0500

    tested apod with postman. is working correctly.

--- src/main/java/com/careerdevs/HelloInternet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/HelloInternet/controllers/NasaController.java
@@ -7,11 +7,15 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/nasa")
+//this request mapping will create a path for all associated route handlers
 public class NasaController {
+
     private final String myNasaKey = "Ra46Z9dcQW0m3hoGcYugzrhHZ1NPY4SmPjP5dN3g";
     private final String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+
+    //this is an individual route handler for apod
     @GetMapping("/apod")
-    public Object apodHandler(RestTemplate restTemplate) {
+    public Object apodHandler (RestTemplate restTemplate) {
         return restTemplate.getForObject(nasaApodEndpoint, Object.class);
     }
 
